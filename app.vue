d5cf0116d2f1daa8148cf08f87cf5ddca3382c1c
Youssef Abdulaziz <azizy5203@gmail.com>
2022-10-25 12:27:04 +0200

    seo optimization

--- app.vue
+++ app.vue
@@ -8,6 +8,10 @@ const router = useRouter();
 
 useHead({
     title: "Rasen Movies",
+    meta:[
+        {name:'description', content:'Box Office latest rankings and reviews'},
+        {name:'twitter:image', content:'./assets/HeroCropped.png'}
+    ],
     link: [
         {
             href: "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
